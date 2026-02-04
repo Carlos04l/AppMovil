bcc6a290a5c028595a69fe20be58a4affabb1a3f
Carlos04l <carloslhdz713@gmail.com>
2026-02-03 18:10:28 -0600

    Delete paginaweb/QAM directory
    BORRAR
    
    Signed-off-by: Carlos04l <carloslhdz713@gmail.com>

--- paginaweb/QAM/test_db.py
+++
@@ -1,8 +0,0 @@
-from config import get_connection
-
-try:
-    conn = get_connection()
-    print("✅ Conexión exitosa a SQL Server")
-    conn.close()
-except Exception as e:
-    print("❌ Error al conectar:", e)
